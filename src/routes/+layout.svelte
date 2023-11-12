67805ecc605938f8df8748d08a70301ae4de1b14
omar <lordbida55@gmail.com>
2023-11-12 21:45:08 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+  import "../app.css";
+  /**@type {}*/
+  let model;
+  const openModel = () => {
+    if (model) {
+      model.showModal();
+    }
+  };
+</script>
+
+<div class="w-full bg-base-200">
+  <div class="navbar max-w-6xl mx-auto">
+    <div class="flex">
+      <a class="text-xl cursor-pointer font-bold text-primary">SHORT URL</a>
+    </div>
+    <div class="flex-1 flex justify-center">
+      <!-- <a class="btn btn-ghost text-xl">Home</a>
+      <a class="btn btn-ghost text-xl">My Links</a> -->
+    </div>
+    <button class="btn btn-ghost text-xl" on:click={openModel}>Login</button>
+  </div>
+</div>
+
+<dialog bind:this={model} id="my_modal_2" class="modal">
+  <div class="modal-box flex flex-col gap-3 p-5 bg-base-200 rounded-lg">
+    <input class="input" placeholder="Email" />
+    <input class="input" placeholder="Password" type="password" />
+    <button class="btn btn-primary">Login</button>
+    <div class="divider">OR</div>
+    <button class="btn btn-neutral">
+      Login with google
+      <img class="w-[40px] h-[40px]" src="googleLogo.png" />
+    </button>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
+<div class="container w-full max-w-3xl mx-auto">
+  <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<div class="bg-base-200 mt-12">
+  <div class="py-8">
+    <h1 class="text-center text-primary font-bold text-3xl">
+      Past the URL to be shortened
+    </h1>
+    <div class="flex flex-col gap-3 sm:flex-row sm:gap-0 p-3 mt-4">
+      <input
+        class="input p-3 rounded-none flex-1 focus:outline-none focus:border-primary"
+        placeholder="Enter URL here"
+      />
+      <button class="btn rounded-none btn-primary">Shorten URL</button>
+    </div>
+    <div class="text-center">
+      <p>Free, easy, secure and fast Short url</p>
+    </div>
+  </div>
+</div>
+<div class="bg-base-200 mt-12">
+  <div class="py-8 flex items-center flex-col">
+    <h1 class="text-center text-primary font-bold text-3xl">
+      Try Premium For more features!
+    </h1>
+    <div class="flex justify-center">
+      <ul class="list-disc mt-5">
+        <li>Create Custom Short Links</li>
+        <li>Track your Short Links with Detailed Analytics</li>
+        <li>Update URLs whenever you need to change their Destination</li>
+      </ul>
+    </div>
+    <button class="btn btn-primary w-max mt-5 rounded-none"
+      >Create Account</button
+    >
+  </div>
+</div>
